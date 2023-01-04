7f4bca811f2e5abd5b75f950bd42a342a8e719bd
poss-ible <1420722664@qq.com>
2023-01-04 22:12:23 +0800

    做好了大部分的迷宫了，还未测试

--- main.cpp
+++ main.cpp
@@ -1,7 +1,54 @@
 #inlcude <bits/stdc++.h>
+#define Tab_Top_Left 1
+#define Tab_Top_Right 2
+#define Tab_Bottom_Left 3
+#define Tab_Bottom_Right 4
+#define Tab_Horizontal 5
+#define Tab_Vertical 6
+#define Blank 7
+#define Black 8
+#define Start 9
+#define End 10
 using namespace std;
 
+void chushihua()
+{
+	srand((unsigned)time(NULL));
+}
+void makemaze(int *a[][],qx,qy,xx,xy)
+{
+	if(!(*a[xx+1][xy]+*a[xx-1][xy]+*a[xx][xy+1]+*a[xx][xy-1]))
+	{
+		*a[xx][xy]=1;
+		*a[qx][xy]=1;
+		map<int,int> b;
+		while(b.size()!=4)
+		{
+			int s=rand()%4;
+			if(!b[s])
+			{
+				b[s]=1;
+				switch(s)
+				{
+					case 0:
+						makemaze(a,xx+1,xy,xx+2,xy);
+						break;
+					case 1:
+						makemaze(a,xx-1,xy,xx-2,xy);
+						break;
+					case 2:
+						makemaze(a,xx,xy+1,xx,xy+2);
+						break;
+					case 3:
+						makemaze(a,xx,xy-1,xx,xy-2);
+				}
+			}
+		}
+	}
+}
 int main()
 {
-	
+	chushihua();
+	int a[19][29]={},x=0,y=0;
+	makemaze(a,x,y,x,y);
 }
