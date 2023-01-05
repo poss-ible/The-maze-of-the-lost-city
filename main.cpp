169e98b64a9b1e002201182a574080bcf93c01d8
poss-ible <1420722664@qq.com>
2023-01-05 16:34:56 +0800

    finish

--- main.cpp
+++ main.cpp
@@ -11,38 +11,44 @@
 #define End 10
 using namespace std;
 
-int a[21][31],ex,ey;
+map<int,map<int,bool>> a;
+int ex,ey;
 void chushihua()
 {
 	srand((unsigned)time(NULL));
 }
 void makemaze(int qx,int qy,int xx,int xy)
 {
-	if((a[xx+1][xy]+a[xx-1][xy]+a[xx][xy+1]+a[xx][xy-1])==0||(ex==xx&&ey==xy))
+	if(!(xx<0||xx>=29||xy<0||xy>=19))
 	{
-		a[xx][xy]=1;
-		a[qx][xy]=1;
-		map<int,int> b;
-		while(b.size()!=4)
+		if((a[xx+1][xy]+a[xx-1][xy]+a[xx][xy+1]+a[xx][xy-1])==0)
 		{
-			int s=rand()%4;
-			if(!b[s])
+			ex=xx;
+			ey=xy;
+			a[xx][xy]=1;
+			a[qx][qy]=1;
+			map<int,int> b;
+			while(b.size()!=4)
 			{
-				b[s]=1;
-				switch(s)
+				int s=rand()%4;
+				if(!b[s])
 				{
-					case 0:
-						makemaze(xx+1,xy,xx+2,xy);
-						break;
-					case 1:
-						makemaze(xx-1,xy,xx-2,xy);
-						break;
-					case 2:
-						makemaze(xx,xy+1,xx,xy+2);
-						break;
-					case 3:
-						makemaze(xx,xy-1,xx,xy-2);
-						break;
+					b[s]=1;
+					switch(s)
+					{
+						case 0:
+							makemaze(xx+1,xy,xx+2,xy);
+							break;
+						case 1:
+							makemaze(xx-1,xy,xx-2,xy);
+							break;
+						case 2:
+							makemaze(xx,xy+1,xx,xy+2);
+							break;
+						case 3:
+							makemaze(xx,xy-1,xx,xy-2);
+							break;
+					}
 				}
 			}
 		}
@@ -51,27 +57,16 @@ void makemaze(int qx,int qy,int xx,int xy)
 int main()
 {
 	chushihua();
-	int x=1,y=1;
-	ex=x;
-	ey=y;
-	for(int i=0;i<21;++i)
-	{
-		a[i][0]=1;
-		a[i][20]=1;
-	}
-	for(int j=0;j<31;++j)
-	{
-		a[0][j]=1;
-		a[30][j]=1;
-	}
+	int x=0,y=0;
 	makemaze(x,y,x,y);
-	for(int i=1;i<=19;++i)
+	for(int i=0;i<19;++i)
 	{
-		for(int j=1;j<=29;++j)
+		for(int j=0;j<29;++j)
 		{
-			cout<<a[i][j];
+			cout<<a[j][i];
 		}
-		cout<<endl;
+		cout<<'\n';
 	}
+	cout<<ex<<' '<<ey;
 	return 0;
 }
