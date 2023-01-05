db35e67ce84883ae8e313415d6e4a0ef54babeda
poss-ible <1420722664@qq.com>
2023-01-05 22:50:00 +0800

    雏形做好啦啊啊啊啊啊啊

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <bits/stdc++.h>
-#define KEY_DOWN(VK_NONAME) ((GetAsyncKeyState(VK_NONAME) & 0x8000) ? 1:0)
+#include <windows.h>
+#include <dir.h>
+#include <conio.h>
+/*
 #define Tab_Top_Left 1
 #define Tab_Top_Right 2
 #define Tab_Bottom_Left 3
@@ -10,15 +13,55 @@
 #define Black 8
 #define Start 9
 #define End 10
+*/
 using namespace std;
 
 map<int,map<int,bool>> a;
 int ex,ey,maxn=0;
+void Main_Screen ()
+{
+	printf("┌──────────────────────────────────────────────────────────┐\n");
+	printf("│                                                          │\n");
+	printf("│                                                          │\n");
+	printf("│                                                          │\n");
+	printf("│                                                          │\n");
+	printf("│                                                          │\n");
+	printf("│                                                          │\n");
+	printf("│                                                          │\n");
+	printf("│                                                          │\n");
+	printf("│                                                          │\n");
+	printf("│                                                          │\n");
+	printf("│                                                          │\n");
+	printf("│                                                          │\n");
+	printf("│                                                          │\n");
+	printf("│                                                          │\n");
+	printf("│                                                          │\n");
+	printf("│                                                          │\n");
+	printf("│                                                          │\n");
+	printf("│                                                          │\n");
+	printf("│                                                          │\n");
+	printf("└──────────────────────────────────────────────────────────┘\n");
+	printf("\n");
+	printf("┌──────────────────────────────────────────────────────────┐\n");
+	printf("│                                                          │\n");
+	printf("├──────────────────────────────────────────────────────────┤\n");
+	printf("│                                                          │\n");
+	printf("│                                                          │\n");
+	printf("│                                                          │\n");
+	printf("│                                                          │\n");
+	printf("└──────────────────────────────────────────────────────────┘\n");
+}
 void chushihua()
 {
 	system ("chcp 65001");
 	system ("cls");
+	//Main_Sqeen();
 	srand((unsigned)time(NULL));
+	HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
+	DWORD mode;
+	GetConsoleMode(hStdin, &mode);
+	mode &= ~ENABLE_QUICK_EDIT_MODE;
+	SetConsoleMode(hStdin, mode);
 }
 void makemaze(int qx,int qy,int xx,int xy,int maxm)
 {
@@ -62,6 +105,14 @@ void makemaze(int qx,int qy,int xx,int xy,int maxm)
 		}
 	}
 }
+void gotoxy(int x,int y)
+{
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD pos;
+	pos.X = x;
+	pos.Y = y;
+	SetConsoleCursorPosition(handle,pos);
+}
 int main()
 {
 	chushihua();
@@ -73,19 +124,19 @@ int main()
 		{
 			if(i==y&&j==x)
 			{
-				cout<<"始";
+				cout<<"s";
 			}
 			else
 			{
 				if(i==ey&&j==ex)
 				{
-					cout<<"终";
+					cout<<"z";
 				}
 				else
 				{
 					if(a[j][i])
 					{
-						cout<<"  ";
+						cout<<" ";
 					}
 					else
 					{
@@ -97,12 +148,78 @@ int main()
 						{
 							cout<<"——";
 						}*/
-						cout<<"■■";
+						cout<<"■";
 					}
 				}
 			}
 		}
 		cout<<'\n';
 	}
+	while(x!=ex||y!=ey)
+	{
+		#define cany(y) (y>=0&&y<19&&a[x][y]!=0)
+		#define canx(x) (x>=0&&x<29&&a[x][y]!=0)
+		char c=getch();
+		switch(c)
+		{
+			case 'a':
+			{
+				--x;
+				if(canx(x))
+				{
+					gotoxy(x,y);
+					cout<<"•";
+				}
+				else
+				{
+					++x;
+				}
+				break;
+			}
+			case 'd':
+			{
+				++x;
+				if(canx(x))
+				{
+					gotoxy(x,y);
+					cout<<"•";
+				}
+				else
+				{
+					--x;
+				}
+				break;
+			}
+			case 'w':
+			{
+				--y;
+				if(cany(y))
+				{
+					gotoxy(x,y);
+					cout<<"•";
+				}
+				else
+				{
+					++y;
+				}
+				break;
+			}
+			case 's':
+			{
+				++y;
+				if(cany(y))
+				{
+					gotoxy(x,y);
+					cout<<"•";
+				}
+				else
+				{
+					--y;
+				}
+				break;
+			}
+		}
+	}
+	cout<<"you win";
 	return 0;
 }
