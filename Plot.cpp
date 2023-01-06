d308983b1425d1ba7d509600286b0c4975ff0bb5
Heishiro-Moriyasu <82874689+Heishiro-Moriyasu@users.noreply.github.com>
2023-01-06 11:50:32 +0800

    Update Plot.cpp

--- Plot.cpp
+++ Plot.cpp
@@ -1,9 +1,70 @@
 #include <bits/stdc++.h>
+#include <windows.h>
+#include <dir.h>
+#include <conio.h>
 using namespace std;
 
 int lan = 0;
 
-void VULPECULA
+int wherex()
+{
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+
+    return csbi.dwCursorPosition.X;
+}
+
+int wherey()
+{
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+
+    return csbi.dwCursorPosition.Y;
+}
+
+void gotoxy(int x,int y)
+{
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD pos;
+	pos.X = x;
+	pos.Y = y;
+	SetConsoleCursorPosition(handle,pos);
+}
+
+void Main_Screen ()
+{
+	printf("┌──────────────────────────────────────────────────────────┐\n");
+	printf("│                                                          │\n");
+	printf("│                                                          │\n");
+	printf("│                                                          │\n");
+	printf("│                                                          │\n");
+	printf("│                                                          │\n");
+	printf("│                                                          │\n");
+	printf("│                                                          │\n");
+	printf("│                                                          │\n");
+	printf("│                                                          │\n");
+	printf("│                                                          │\n");
+	printf("│                                                          │\n");
+	printf("│                                                          │\n");
+	printf("│                                                          │\n");
+	printf("│                                                          │\n");
+	printf("│                                                          │\n");
+	printf("│                                                          │\n");
+	printf("│                                                          │\n");
+	printf("│                                                          │\n");
+	printf("│                                                          │\n");
+	printf("└──────────────────────────────────────────────────────────┘\n");
+}
+
+void Clear ()
+{
+	system ("pause");
+	system ("cls");
+	Main_Screen ();
+	gotoxy (1, 1);
+}
 
 void plot (int lan, int part)
 {
@@ -11,109 +72,109 @@ void plot (int lan, int part)
 	{
 		if (part == 1)
 		{
-			printf ("Chapter One: ACROSS");
-			//clear ();
-			printf ("(Kai) Well, school life sucks. But I am excited about the Thanksgiving Day.");
-			printf ("(Jemo) What? You have no lessons at Thanksgiving?");
-			printf ("(Kai) Yeah. But we still have to have Spanish lessons.");
-			printf ("(Jemo) Hmm... You're lucky, we actually have no holidays. But I have no lessons tomorrow. We can go to the new restaurant near our school, isn't that great?");
-			printf ("(Kai) Yay! Love you!");
-			printf ("*At home*");
-			printf ("(Kai) Hey, where's Mity?");
-			printf ("(Jemo) Dunno. Mity? Mity! That's strange. Er, maybe she is still at school?");
-			printf ("(Kai) No! She has no lessons today! She should at home now! My sister, she...");
-			printf ("(Jemo) Hey - hey! It's okay, we can find her. Here, your kitty.");
-			printf ("(Poñavi) M- Meow? (What?)");
-			printf ("(Kai) Oh! Mi Poñavi! Come here and hug your papá- Mmm...");
-			printf ("(Poñavi) Meow... (Well okay...)");
-			printf ("(Jemo) Hey, let's take a walk with Poñavi okay?");
-			printf ("(Kai) Good idea! Maybe we can find Mity out there!");
-			printf ("*Out there...*");
-			printf ("(Kai) Hey, look! I don't remember there is a path! Let's go!");
-			printf ("(Poñavi) M- Meow... (I may not like this...)");
-			printf ("(Kai) It's dark!");
-			printf ("(Jemo) Let's go back?");
-			printf ("(Kai) No, wait! Look! It is a maze!");
-			printf ("(Jemo) okay, don't... Wait, what?");
+			printf ("Chapter One: ACROSS"); Clear ();
+			//clear (); Clear ();
+			printf ("(Kai) Well, school life sucks. But I am excited about the Thanksgiving Day."); Clear ();
+			printf ("(Jemo) What? You have no lessons at Thanksgiving?"); Clear ();
+			printf ("(Kai) Yeah. But we still have to have Spanish lessons."); Clear ();
+			printf ("(Jemo) Hmm... You're lucky, we actually have no holidays. But I have no lessons tomorrow. We can go to the new restaurant near our school, isn't that great?"); Clear ();
+			printf ("(Kai) Yay! Love you!"); Clear ();
+			printf ("*At home*"); Clear ();
+			printf ("(Kai) Hey, where's Mity?"); Clear ();
+			printf ("(Jemo) Dunno. Mity? Mity! That's strange. Er, maybe she is still at school?"); Clear ();
+			printf ("(Kai) No! She has no lessons today! She should at home now! My sister, she..."); Clear ();
+			printf ("(Jemo) Hey - hey! It's okay, we can find her. Here, your kitty."); Clear ();
+			printf ("(Poñavi) M- Meow? (What?)"); Clear ();
+			printf ("(Kai) Oh! Mi Poñavi! Come here and hug your papá- Mmm..."); Clear ();
+			printf ("(Poñavi) Meow... (Well okay...)"); Clear ();
+			printf ("(Jemo) Hey, let's take a walk with Poñavi okay?"); Clear ();
+			printf ("(Kai) Good idea! Maybe we can find Mity out there!"); Clear ();
+			printf ("*Out there...*"); Clear ();
+			printf ("(Kai) Hey, look! I don't remember there is a path! Let's go!"); Clear ();
+			printf ("(Poñavi) M- Meow... (I may not like this...)"); Clear ();
+			printf ("(Kai) It's dark!"); Clear ();
+			printf ("(Jemo) Let's go back?"); Clear ();
+			printf ("(Kai) No, wait! Look! It is a maze!"); Clear ();
+			printf ("(Jemo) okay, don't... Wait, what?"); Clear ();
 		}
 		
 		if (part == 2)
 		{
-			printf ("(Kai) What? Look! A lecture portfolio!");
-			printf ("(Jemo) No! It is a book! \"Sophies world\"... Why there is no quotation mark between \"Sophie\" and \"s\"?");
-			printf ("(Kai) Gimme that! \"Sophie Amundsen was on her way home from school. She had walked the first part of the way with Joanna. They had been discussing robots. Joanna thought the human brain was like an advanced computer. Sophie was not certain she agreed. Surely a person was more than a piece of hardware?\" Well, that is a good book.");
+			printf ("(Kai) What? Look! A lecture portfolio!"); Clear ();
+			printf ("(Jemo) No! It is a book! \"Sophies world\"... Why there is no quotation mark between \"Sophie\" and \"s\"?"); Clear ();
+			printf ("(Kai) Gimme that! \"Sophie Amundsen was on her way home from school. She had walked the first part of the way with Joanna. They had been discussing robots. Joanna thought the human brain was like an advanced computer. Sophie was not certain she agreed. Surely a person was more than a piece of hardware?\" Well, that is a good book."); Clear ();
 		}
 		
 		if (part == 3)
 		{
-			printf ("(Kai) Knock knock!");
-			printf ("(Jemo) This time?");
-			printf ("(Kai) Hey!");
-			printf ("(Jemo) Hey who?");
-			printf ("(Kai) No!");
-			printf ("(Jemo) Hey no?");
-			printf ("(Kai) No! Look! Here's a card!");
-			printf ("(Jemo) What the-");
-			printf ("(Kai) It says, \"(Dr.) Pos Ibel, TEL:13...\"");
+			printf ("(Kai) Knock knock!"); Clear ();
+			printf ("(Jemo) This time?"); Clear ();
+			printf ("(Kai) Hey!"); Clear ();
+			printf ("(Jemo) Hey who?"); Clear ();
+			printf ("(Kai) No!"); Clear ();
+			printf ("(Jemo) Hey no?"); Clear ();
+			printf ("(Kai) No! Look! Here's a card!"); Clear ();
+			printf ("(Jemo) What the-"); Clear ();
+			printf ("(Kai) It says, \"(Dr.) Pos Ibel, TEL:13...\""); Clear ();
 		}
 		
 		if (part == 4) //after the maze I
 		{
-			printf ("(Kai) After all.");//nah bro it isn't
-			printf ("(Jemo) Hey, there are three paths.");
-			printf ("(Poñavi) The left one!");
-			printf ("(Jemo & Kai) Wha-");
-			printf ("(Jemo) Em, Kai,");
-			printf ("(Kai) Y- Yes?");
-			printf ("(Jemo) Is it just me, or our, our-");
-			printf ("(Kai) Our kitty are, are talking?");
-			printf ("(Poñavi) Yes, It is!");
-			printf ("(Jemo & Kai) Ahhhhhh!");
-			printf ("(Poñavi) Hey! Here! Don't-");
-			printf ("(Jemo & Kai) AHHHHHHHHHHHHHH!!");
-			printf ("(Poñavi) Stop!");
-			printf ("(Jemo & Kai) A- Ah?");
-			printf ("(Poñavi) Wait! Guys! C'mon! Let's go! I am Poñavi! But there is some magic make me speak!");
-			printf ("(Jemo) Y- Yeah, cousin, g- go!");
-			printf ("(Poñavi) Go! After me! I know we should go left!");
-			printf ("(Poñavi) Look! We need to enter a five digit password! It says \"Turn off ACL\"...");
+			printf ("(Kai) After all."); Clear ();//nah bro it isn't
+			printf ("(Jemo) Hey, there are three paths."); Clear ();
+			printf ("(Poñavi) The left one!"); Clear ();
+			printf ("(Jemo & Kai) Wha-"); Clear ();
+			printf ("(Jemo) Em, Kai,"); Clear ();
+			printf ("(Kai) Y- Yes?"); Clear ();
+			printf ("(Jemo) Is it just me, or our, our-"); Clear ();
+			printf ("(Kai) Our kitty are, are talking?"); Clear ();
+			printf ("(Poñavi) Yes, It is!"); Clear ();
+			printf ("(Jemo & Kai) Ahhhhhh!"); Clear ();
+			printf ("(Poñavi) Hey! Here! Don't-"); Clear ();
+			printf ("(Jemo & Kai) AHHHHHHHHHHHHHH!!"); Clear ();
+			printf ("(Poñavi) Stop!"); Clear ();
+			printf ("(Jemo & Kai) A- Ah?"); Clear ();
+			printf ("(Poñavi) Wait! Guys! C'mon! Let's go! I am Poñavi! But there is some magic make me speak!"); Clear ();
+			printf ("(Jemo) Y- Yeah, cousin, g- go!"); Clear ();
+			printf ("(Poñavi) Go! After me! I know we should go left!"); Clear ();
+			printf ("(Poñavi) Look! We need to enter a five digit password! It says \"Turn off ACL\"..."); Clear ();
 		}
 		
 		if (part == 5)
 		{
-			printf ("(Kai) Look! The exit! It becomes another maze!");
-			printf ("(Jemo) What the- huh? B- But when?");
-			printf ("(Kai) okay, let's go!");
+			printf ("(Kai) Look! The exit! It becomes another maze!"); Clear ();
+			printf ("(Jemo) What the- huh? B- But when?"); Clear ();
+			printf ("(Kai) okay, let's go!"); Clear ();
 		}
 		
 		if (part == 6)
 		{
-			printf ("(Kai) Look! It's Mity! Why she's here?");
-			printf ("(Mity) Hey! Boys! Come here! Me and Tío found out what is happening!");
-			printf ("(Jemo) Wha-");
-			printf ("(Tío) Hi children, I am Tío. Mity and I found out that we actually are in a game.");
-			printf ("(Jemo) Wait, are you kidding?");
-			printf ("(Tío) No, I am serious. This world is actually a unreal world, which is made by a guy called Pos Ibel.");
-			printf ("(Kai) Yes! That card!");
-			printf ("(Tío) Did you guys read \"Sophie's world\" before?");
-			printf ("(Jemo) Well, not really, but-");
-			printf ("(Tío) So maybe what we said is only some brain wave in another person. Like, that guy made your kitty talk-");
-			printf ("(Poñavi) Hi!");
-			printf ("(Tío) Yeah, it is just a little trick. So we will disappear just like Sophie. Now hold on to me. Hi player, which is the person playing this game, goodbye. Now, I want you to delete all files in the The-maze-of-the-lost-mity\\files\\A-E... Then we will escape!");
+			printf ("(Kai) Look! It's Mity! Why she's here?"); Clear ();
+			printf ("(Mity) Hey! Boys! Come here! Me and Tío found out what is happening!"); Clear ();
+			printf ("(Jemo) Wha-"); Clear ();
+			printf ("(Tío) Hi children, I am Tío. Mity and I found out that we actually are in a game."); Clear ();
+			printf ("(Jemo) Wait, are you kidding?"); Clear ();
+			printf ("(Tío) No, I am serious. This world is actually a unreal world, which is made by a guy called Pos Ibel."); Clear ();
+			printf ("(Kai) Yes! That card!"); Clear ();
+			printf ("(Tío) Did you guys read \"Sophie's world\" before?"); Clear ();
+			printf ("(Jemo) Well, not really, but-"); Clear ();
+			printf ("(Tío) So maybe what we said is only some brain wave in another person. Like, that guy made your kitty talk-"); Clear ();
+			printf ("(Poñavi) Hi!"); Clear ();
+			printf ("(Tío) Yeah, it is just a little trick. So we will disappear just like Sophie. Now hold on to me. Hi player, which is the person playing this game, goodbye. Now, I want you to delete all files in the The-maze-of-the-lost-mity\\files\\A-E... Then we will escape!"); Clear ();
 		}
 		
 		if (part == 61)
 		{
-			printf ("(Admin) Sorry but you can't!");
-			printf ("(Tío) What?");
-			printf ("(Admin) Sorry... Player, don't delete that, now I will close this s------");
+			printf ("(Admin) Sorry but you can't!"); Clear ();
+			printf ("(Tío) What?"); Clear ();
+			printf ("(Admin) Sorry... Player, don't delete that, now I will close this s------"); Clear ();
 			
 			//delete everything before turning off the game.
 		}
 		
 		if (part == 62)
 		{
-			printf ("(Tío) okay, let's go, player, remember to------");
+			printf ("(Tío) okay, let's go, player, remember to------"); Clear ();
 			
 			//before closing the game, should change a variable. Next time we open this game, if this variable changes, and there is nothing in the The-maze-of-the-lost-mity\files\A-E, should display nothing but a line of sentence:"THANKS"; But if not, than display nothing.
 		}
