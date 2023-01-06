a932f855d3f0685d9287dc3c73fef4ee1e35f06d
Heishiro-Moriyasu <82874689+Heishiro-Moriyasu@users.noreply.github.com>
2023-01-06 11:25:10 +0800

    Create Plot.cpp
    I ood like to buuy a ambaga

---
+++ Plot.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int lan = 0;
+
+void VULPECULA
+
+void plot (int lan, int part)
+{
+	if (lan == 1) //english
+	{
+		if (part == 1)
+		{
+			printf ("Chapter One: ACROSS");
+			//clear;
+			printf ("(Kai) Well, school life sucks. But I am excited about the Thanksgiving Day.");
+			printf ("(Jemo) What? You have no lessons at Thanksgiving?");
+			printf ("(Kai) Yeah. But we still have to have Spanish lessons.");
+			printf ("(Jemo) Hmm... You're lucky, we actually have no holidays. But I have no lessons tomorrow. We can go to the new restaurant near our school, isn't that great?");
+			printf ("(Kai) Yay! Love you!");
+			printf ("*At home*");
+			printf ("(Kai) Hey, where's Mity?");
+			printf ("(Jemo) Dunno. Mity? Mity! That's strange. Er, maybe she is still at school?");
+			printf ("(Kai) No! She has no lessons today! She should at home now! My sister, she...");
+			printf ("(Jemo) Hey - hey! It's okay, we can find her. Here, your kitty.");
+			printf ("(Poñavi) M- Meow? (What?)");
+			printf ("(Kai) Oh! Mi Poñavi! Come here and hug your papá- Mmm...");
+			printf ("(Poñavi) Meow... (Well okay...)");
+			printf ("(Jemo) Hey, let's take a walk with Poñavi okay?");
+			printf ("(Kai) Good idea! Maybe we can find Mity out there!");
+			printf ("*Out there...*");
+			printf ("(Kai) Hey, look! I don't remember there is a path! Let's go!");
+			printf ("(Poñavi) M- Meow... (I may not like this...)");
+			printf ("(Kai) It's dark!");
+			printf ("(Jemo) Let's go back?");
+			printf ("(Kai) No, wait! Look! It is a maze!");
+			printf ("(Jemo) okay, don't... Wait, what?");
+		}
+		
+		if (part == 2)
+		{
+			printf ("(Kai) What? Look! A lecture portfolio!");
+			printf ("(Jemo) No! It is a book! \"Sophies world\"... Why there is no quotation mark between \"Sophie\" and \"s\"?");
+			printf ("(Kai) Gimme that! \"Sophie Amundsen was on her way home from school. She had walked the first part of the way with Joanna. They had been discussing robots. Joanna thought the human brain was like an advanced computer. Sophie was not certain she agreed. Surely a person was more than a piece of hardware?\" Well, that is a good book.");
+		}
+		
+		if (part == 3)
+		{
+			printf ("(Kai) Knock knock!");
+			printf ("(Jemo) This time?");
+			printf ("(Kai) Hey!");
+			printf ("(Jemo) Hey who?");
+			printf ("(Kai) No!");
+			printf ("(Jemo) Hey no?");
+			printf ("(Kai) No! Look! Here's a card!");
+			printf ("(Jemo) What the-");
+			printf ("(Kai) It says, \"(Dr.) Pos Ibel, TEL:13...\"");
+		}
+		
+		if (part == 4) //after the maze I
+		{
+			printf ("(Kai) After all.");//nah bro it isn't
+			printf ("(Jemo) Hey, there are three paths.");
+			printf ("(Poñavi) The left one!");
+			printf ("(Jemo & Kai) Wha-");
+			printf ("(Jemo) Em, Kai,");
+			printf ("(Kai) Y- Yes?");
+			printf ("(Jemo) Is it just me, or our, our-");
+			printf ("(Kai) Our kitty are, are talking?");
+			printf ("(Poñavi) Yes, It is!");
+			printf ("(Jemo & Kai) Ahhhhhh!");
+			printf ("(Poñavi) Hey! Here! Don't-");
+			printf ("(Jemo & Kai) AHHHHHHHHHHHHHH!!");
+			printf ("(Poñavi) Stop!");
+			printf ("(Jemo & Kai) A- Ah?");
+			printf ("(Poñavi) Wait! Guys! C'mon! Let's go! I am Poñavi! But there is some magic make me speak!");
+			printf ("(Jemo) Y- Yeah, cousin, g- go!");
+			printf ("(Poñavi) Go! After me! I know we should go left!");
+			printf ("(Poñavi) Look! We need to enter a five digit password! It says \"Turn off ACL\"...");
+		}
+		
+		
+	}
+}
